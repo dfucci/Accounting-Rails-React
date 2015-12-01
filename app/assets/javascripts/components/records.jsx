e4022aeab90774fe9f92ec9488efcb559cb16ea7
dfucci <davfuc@gmail.com>
2015-12-01 22:47:02 +0200

    initial commit

---
+++ app/assets/javascripts/components/records.jsx
@@ -0,0 +1,40 @@
+this.Records = React.createClass({
+  getInitialState(){
+    return({
+      records: this.props.data
+    })
+  },
+
+  getDefaultProps(){
+    return({
+      records: []
+    })
+  },
+
+  renderRecords(){
+    return this.state.records.map((record)=>{
+      return <Record key={record.id} record={record} />;
+    });
+  },
+
+  render(){
+    return (
+      <div className='records'>
+        <h2 className='title'>Records</h2>
+        <table className='table table-bordered'>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Title</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderRecords()}
+          </tbody>
+        </table>
+      </div>
+    )
+
+  }
+});
