1a8ea580b1b041d6ab7f1d2cb4c64e0a0f818a4b
dfucci <davfuc@gmail.com>
2015-12-03 00:13:00 +0200

    submit form

--- app/assets/javascripts/components/record_form.jsx
+++ app/assets/javascripts/components/record_form.jsx
@@ -1,46 +1,63 @@
 RecordForm = React.createClass({
-  getInitialState(){
-    return {
-      title: '',
-      date: '',
-      amount: ''
+    getInitialState(){
+        return {
+            title: '',
+            date: '',
+            amount: ''
+        }
+    },
+    handleChange(e){
+        let name = e.target.name;
+        let state ={};
+        state[name]  = e.target.value;
+        this.setState(state);
+    },
+    valid(){
+        return this.state.title && this.state.date && this.state.amount;
+    },
+
+    handleSubmit(e){
+        e.preventDefault();
+        console.log("submitting");
+        $.post('', {record: this.state}, (data)=>{
+            this.props.handleNewRecord(data);
+            this.setState(this.getInitialState());
+        }, 'JSON');
+    },
+
+    render(){
+        return(
+            <form className="form-inline">
+                <div className="form-group" onSubmit={this.handleSubmit}>
+                    <input type="text"
+                        placeholder="Date"
+                        name="date"
+                        value={this.state.date}
+                        onChange={this.handleChange}
+                        className="form-control"/>
+                </div>
+                <div className="form-group">
+                    <input type="text"
+                        placeholder="Title"
+                        name="title"
+                        value={this.state.title}
+                        onChange={this.handleChange}
+                        className="form-control"/>
+                </div>
+                <div className="form-group">
+                    <input type="text"
+                        placeholder="Amount"
+                        name="amount"
+                        value={this.state.amount}
+                        onChange={this.handleChange}
+                        className="form-control"/>
+                </div>
+                <button type='submit'
+                    className='btn btn-primary'
+                    disabled={!this.valid()}>
+                    Create record
+                </button>
+            </form>
+        )
     }
-  },
-  handleChange(){},
-  valid(){},
-  render(){
-    return(
-        <form className="form-inline">
-          <div className="form-group">
-            <input type="text"
-              placeholder="Date"
-              name="Date"
-              value={this.state.date}
-              jonChange={this.handleChange}
-              className="form-control"/>
-            </div>
-          <div className="form-group">
-            <input type="text"
-              placeholder="Title"
-              name="Title"
-              value={this.state.title}
-              onChange={this.handleChange}
-              className="form-control"/>
-            </div>
-          <div className="form-group">
-            <input type="text"
-              placeholder="Amount"
-              name="Amount"
-              value={this.state.amount}
-              onChange={this.handleChange}
-              className="form-control"/>
-            </div>
-            <button type='submit'
-              className='btn btn-primary'
-              disabled={!this.valid()}>
-                Create record
-              </button>
-        </form>
-    )
-  }
 });
